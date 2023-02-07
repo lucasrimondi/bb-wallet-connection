6de2e7d8630215e8fe86e1b706170e526efd7a34
Lucas Rimondi <lucasrimondi@MacBook-Air-de-Lucas.local>
2023-02-07 18:01:49 -0300

    adding connector and logic

--- src/components/wallet/connectors.tsx
+++ src/components/wallet/connectors.tsx
@@ -1,7 +1,19 @@
 import { InjectedConnector } from "@web3-react/injected-connector";
+import { WalletConnectConnector } from "@web3-react/walletconnect-connector";
 
+//injected connector is the connector for Metamask
 export const injected = new InjectedConnector({
   supportedChainIds: [1, 3, 4, 5, 42],
 });
 
-//injected connector is the connector for Metamask
+export const walletconnect = new WalletConnectConnector({
+  // rpc: { [constants.chainId()]: constants.rpcUrl() },
+  // chainId: constants.chainId(),
+  // supportedChainIds: [constants.chainId()],
+  qrcode: true,
+  bridge: "https://walletconnect-bridge.rifos.org/",
+  qrcodeModalOptions: {
+    mobileLinks: ["metamask", "defiant", "xcapit"],
+    desktopLinks: [],
+  },
+});
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import Head from "next/head";
 import styles from "@/styles/Home.module.css";
 import { useWeb3React } from "@web3-react/core";
 //'injected' is the name of the metamask connector
-import { injected } from "../components/wallet/connectors";
+import { injected, walletconnect } from "../components/wallet/connectors";
 
 export default function Home() {
   //this is a hook that provides context from web3 connection. Thats why you have to wrap your app (_app.tsx) in web3 provider
@@ -12,6 +12,14 @@ export default function Home() {
   async function connectMetamask() {
     try {
       await activate(injected);
+    } catch (ex) {
+      console.log(ex);
+    }
+  }
+
+  async function connectWalletConnect() {
+    try {
+      await activate(walletconnect);
     } catch (ex) {
       console.log(ex);
     }
@@ -38,11 +46,8 @@ export default function Home() {
           <button className={styles.login} onClick={connectMetamask}>
             <h6>Connect Metamask</h6>
           </button>
-          <button
-            className={styles.login}
-            onClick={() => console.log("wallet connect")}
-          >
-            <h6>Connect WalletConnect</h6>
+          <button className={styles.login} onClick={connectWalletConnect}>
+            <h6>WalletConnect</h6>
           </button>
           {active ? (
             <>
