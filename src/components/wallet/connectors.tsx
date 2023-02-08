a32ce2cbe5af6a1898d0207a2544dd7bd4e0ec26
Lucas Rimondi <lucasrimondi@MacBook-Air-de-Lucas.local>
2023-02-08 12:58:46 -0300

    investigating WalletConnectConnector variables

--- src/components/wallet/connectors.tsx
+++ src/components/wallet/connectors.tsx
@@ -6,14 +6,13 @@ export const injected = new InjectedConnector({
   supportedChainIds: [1, 3, 4, 5, 42],
 });
 
+
 export const walletconnect = new WalletConnectConnector({
   // rpc: { [constants.chainId()]: constants.rpcUrl() },
   // chainId: constants.chainId(),
   // supportedChainIds: [constants.chainId()],
   qrcode: true,
   bridge: "https://walletconnect-bridge.rifos.org/",
-  qrcodeModalOptions: {
-    mobileLinks: ["metamask", "defiant", "xcapit"],
-    desktopLinks: [],
-  },
 });
+
+
