3d1916e04daee2d9230f0d72bfe22333cdb356fa
Lucas Rimondi <lucasrimondi@MacBook-Air-de-Lucas.local>
2023-02-08 16:29:35 -0300

    cleaning code to start testing new lib

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Web3ReactProvider } from "@web3-react/core";
-import Web3 from "web3";
-//web3 library facilitates communications with the nodes. (you can alternatively use ethers.js)
 
-function getLibrary(provider: any) {
-  return new Web3(provider);
-}
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <Web3ReactProvider getLibrary={getLibrary}>
-      <Component {...pageProps} />
-    </Web3ReactProvider>
-  );
+  return <Component {...pageProps} />;
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,8 @@
 import Head from "next/head";
 import styles from "@/styles/Home.module.css";
-import { useWeb3React } from "@web3-react/core";
-//'injected' is the name of the metamask connector
-import { injected } from "../components/wallet/connectors";
+
 
 export default function Home() {
-  //this is a hook that provides context from web3 connection. Thats why you have to wrap your app (_app.tsx) in web3 provider
-  const { active, account, library, connector, activate, deactivate } =
-    useWeb3React();
-
-  async function connectMetamask() {
-    try {
-      await activate(injected);
-    } catch (ex) {
-      console.log(ex);
-    }
-  }
-
-  async function disconnect() {
-    try {
-      deactivate();
-    } catch (ex) {
-      console.log(ex);
-    }
-  }
-
   return (
     <>
       <Head>
@@ -35,27 +13,27 @@ export default function Home() {
       </Head>
       <main className={styles.main}>
         <div className={styles.metamaskContainer}>
-          <button className={styles.login} onClick={connectMetamask}>
-            <h6>Connect Metamask</h6>
+          <button
+            className={styles.login}
+            onClick={() => console.log("probando nueva lib")}
+          >
+            <h6>Connect Wallet</h6>
           </button>
-          {active ? (
+          {false ? (
             <>
               <span>
-                Connected with <b>{account}</b>
+                Connected with <b>{"account"}</b>
               </span>
-              <button className={styles.login} onClick={disconnect}>
+              <button
+                className={styles.login}
+                onClick={() => console.log("probando nueva lib")}
+              >
                 <h6>Disconnect</h6>
               </button>
             </>
           ) : (
             <span>Not connected</span>
           )}
-          {/* <button
-              className={styles.login}
-              onClick={() => console.log('connect wallet')}
-            >
-              <h6>Wallet Connect</h6>
-            </button> */}
         </div>
       </main>
     </>
